ef4d795e2019ac44aa9ae6d5b58b31d8f590fb0b
SanskarGosavi2003 <210100132@iitb.ac.in>
2023-04-11 14:20:41 +0530

    First Code Commit: Setup the initial codebase with all the required libraries installed Connected the codebase to the MongoDB Database Version: 1.0.0

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
+    // Connect to MongoDB
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI) 
+        console.log(`MongoDB Connected: ${conn.connection.host}`)
+    }
+    // If there is an error, log it and exit the process
+    catch (err) {
+        console.error(err) 
+        process.exit(1) // Exit with failure
+    }
+}
+// Export the function so that it can be used in other files
+module.exports = connectDB
---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const dotenv = require('dotenv').config() //config() returns an object with a parsed key containing the loaded content or an error key if it failed. A parsed key is an object with key/value pairs where the values are strings or arrays. https://www.npmjs.com/package/dotenv
+const connectDB = require('./config/db')
+const port = process.env.PORT || 5000
+
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false })) //what is urlencoded?: https://stackoverflow.com/questions/29960764/what-does-extended-mean-in-express-4-0s-router-options
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
