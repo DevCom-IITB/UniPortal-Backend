fb003119e6ee0109620dc0ea6abd0d76a0a1b84e
Amit <amitmalakar887@gmail.com>
2023-05-19 23:24:34 +0530

    Add user_Name field for question,answer and comment model

--- api/controllers/elasticController.js
+++ api/controllers/elasticController.js
@@ -1,77 +1,71 @@
-const asyncHandler = require('express-async-handler')
-const fs = require('fs')
-const { Client } = require('@elastic/elasticsearch')
+const asyncHandler = require("express-async-handler");
+const fs = require("fs");
+const { Client } = require("@elastic/elasticsearch");
 const client = new Client({
-    node: process.env.ELASTIC_URI,
-    auth:{
-        "username":process.env.ELASTIC_USERNAME,
-        "password":process.env.ELASTIC_PASSWORD
-    },
+  node: process.env.ELASTIC_URI,
+  auth: {
+    username: process.env.ELASTIC_USERNAME,
+    password: process.env.ELASTIC_PASSWORD,
+  },
   tls: {
-    rejectUnauthorized: false
-  }
-})
-
-
-
-
-
-
+    rejectUnauthorized: false,
+  },
+});
 
 const searchDoc = asyncHandler(async (req, res) => {
-    const a =req.body.search
-    await client.search({
-        index: "infoques",
-        query: {
-            bool: {
-                should: [
-                    {
-                        match: {
-                            doc_title: {
-                                query: a//query string goes here
-                            }
-                        }
-                    },
-                    {
-                        fuzzy:{
-                            doc_title:{
-                                value:a,
-                                fuzziness:2
-                            }
-                        }
-                    }
-                ]
-            }
-        }
-    }).then(data => {
-        data.hits.hits.forEach(element => console.log(element))
-        res.json(data)
+  const a = req.body.search;
+  await client
+    .search({
+      index: "infoques",
+      query: {
+        bool: {
+          should: [
+            {
+              match: {
+                doc_title: {
+                  query: a, //query string goes here
+                },
+              },
+            },
+            {
+              fuzzy: {
+                doc_title: {
+                  value: a,
+                  fuzziness: 2,
+                },
+              },
+            },
+          ],
+        },
+      },
     })
-        .catch(err => console.log(err))
-})
-
-
-
-const indexDoc = async (body,id) => {
-    await client.index({
-        index: 'infoques',
-        id:id, // setting a custom id which is equal to that of the equivalent question
-        body: {
-            doc_title: body //title
-        }
-    }).catch((err) => {
-        console.log(err)
+    .then((data) => {
+      data.hits.hits.forEach((element) => console.log(element));
+      res.json(data);
     })
-    console.log("success")
-}
+    .catch((err) => console.log(err));
+});
 
-
-const deleteDoc = async(id)=>{
-    await client.delete({
-        index:'infoques',
-        id:id
+const indexDoc = async (body, id) => {
+  await client
+    .index({
+      index: "infoques",
+      id: id, // setting a custom id which is equal to that of the equivalent question
+      body: {
+        doc_title: body, //title
+      },
     })
-}
+    .catch((err) => {
+      console.log(err);
+    });
+  console.log("success");
+};
 
+const deleteDoc = async (id) => {
+  await client.delete({
+    index: "infoques",
+    id: id,
+  });
+};
 
-module.exports={indexDoc,searchDoc,deleteDoc}
+module.exports = { indexDoc, searchDoc, deleteDoc };
